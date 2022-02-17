f95ecc76535dbfd69522ffe678f4e2afe935730f
kratosmemes <angelpadillasanchez@hotmail.com>
2022-02-17 17:10:15 -0600

    First commit

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {        
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+    
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path="/{id}")
+    public Optional<UsuarioModel> obtenerUsuariosPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+    
+}
